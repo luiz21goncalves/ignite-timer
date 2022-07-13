bcf86c1c12af2e9e5923611f877b4cb9b0352c31
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-07-12 21:04:23 -0300

    feat: add header content

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,23 @@
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+import logoIgnite from '../../assets/logo-ignite.svg'
+
+import * as S from './styles'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <S.HeaderContainter>
+      <img src={logoIgnite} alt="" />
+
+      <nav>
+        <NavLink to="/" title="Cronômetro">
+          <Timer size={24} />
+        </NavLink>
+
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </S.HeaderContainter>
+  )
 }
