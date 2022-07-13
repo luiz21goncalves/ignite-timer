ad4464474d90e459e4f819e5397dc44791df5417
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-07-12 21:58:59 -0300

    feat: add home content

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,42 @@
+import { Play } from 'phosphor-react'
+
+import * as S from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <S.HomeContainer>
+      <form>
+        <S.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <S.TaskInput
+            id="task"
+            type="text"
+            placeholder="Dê um nome para seu projeto"
+          />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <S.MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+          />
+
+          <span>minutos.</span>
+        </S.FormContainer>
+
+        <S.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountdownContainer>
+
+        <S.StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </S.StartCountdownButton>
+      </form>
+    </S.HomeContainer>
+  )
 }
